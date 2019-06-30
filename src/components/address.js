8761362dd81296150acae50fa703f89d4734fa35
marcuspereiradev <vinnycoast@gmail.com>
2019-06-30 14:31:08 -0300

    add components

---
+++ src/components/address.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Address = ({ address }) => (
+  <table>
+    <tbody>
+      {console.log(address)}
+      <tr>
+        <th>Logradouro/Nome:</th>
+        <td>{address.logradouro}</td>
+      </tr>
+      <tr>
+        <th>Bairro/Distrito:</th>
+        <td>{address.bairro}</td>
+      </tr>
+      <tr>
+        <th>Localidade/UF:</th>
+        <td>{address.cidade}/{address.estado}</td>
+      </tr>
+      <tr>
+        <th>CEP:</th>
+        <td>{address.cep}</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+Address.propTypes = {
+  address: PropTypes.object
+}
+
+export default Address;
