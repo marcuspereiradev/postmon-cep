4829c9cb429848d90fcc3312646dbded29489f50
marcuspereiradev <vinnycoast@gmail.com>
2019-06-30 14:29:17 -0300

    fetch in API

---
+++ src/services/PostmonAPI.js
@@ -0,0 +1,15 @@
+class PostmonAPI {
+  static async fetchAddress(cep) {
+    try {
+      const response = await fetch(`https://api.postmon.com.br/v1/cep/${cep}`);
+      const data = await response.json();
+      return data;
+    }
+    catch(error) {
+      console.error(`Something is wrong in fetchAddress ${error}`);
+      return {};
+    }
+  }
+};
+
+export default PostmonAPI;
