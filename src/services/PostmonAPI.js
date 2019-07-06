9d0adb36eaf8f2af56eb23e48d73c60c25e19b4b
marcuspereiradev <vinnycoast@gmail.com>
2019-07-06 18:24:49 -0300

    When await is the last line of the function we dont need to explicit, just return

--- src/services/PostmonAPI.js
+++ src/services/PostmonAPI.js
@@ -2,8 +2,7 @@ class PostmonAPI {
   static async fetchAddress(cep) {
     try {
       const response = await fetch(`https://api.postmon.com.br/v1/cep/${cep}`);
-      const data = await response.json();
-      return data;
+      return response.json();
     }
     catch(error) {
       console.error(`Something is wrong in fetchAddress ${error}`);
