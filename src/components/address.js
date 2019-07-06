d349fdb24d62557b4bccdb0a9a61c4fc96c71939
marcuspereiradev <vinnycoast@gmail.com>
2019-07-06 19:44:16 -0300

    Changing the order of items

--- src/components/address.js
+++ src/components/address.js
@@ -6,14 +6,12 @@ import '../styles/address.scss';
 const Address = ({ address }) => (
 
   <div className='address-content'>
-    <label>Logradouro/Nome:</label>
+    <label>Logradouro / Nome:</label>
     <p>- {address.logradouro}</p>
-    <label>Bairro/Distrito:</label>
-    <p>- {address.bairro}</p>
-    <label>Cidade:</label>
-    <p>- {address.cidade}</p>
-    <label>Estado/UF:</label>
-    <p>- {address.estado_info.nome}/{address.estado}</p>
+    <label>Bairro / Cidade:</label>
+    <p>- {address.bairro} / {address.cidade}</p>
+    <label>Estado / UF:</label>
+    <p>- {address.estado_info.nome} / {address.estado}</p>
     <label>CEP:</label>
     <p>- {address.cep}</p>
   </div>
